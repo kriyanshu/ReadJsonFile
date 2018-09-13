01f7eb8a4c1d714ac536d4056458607b5fdbbe98
developer <nishant.itworks@gmail.com>
2018-09-13 17:43:26 +0530

    first comit

---
+++ ReadjsonFiel/cells/firstTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  firstTableViewCell.swift
+//  ReadjsonFiel
+//
+//  Created by developer on 13/09/18.
+//  Copyright © 2018 dwllitedemo. All rights reserved.
+//
+
+import UIKit
+
+class firstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
